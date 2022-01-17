123b0afd9fe57a561241c3df28101c20e0967854
czlingo <czlingo@gmail.com>
2022-01-17 09:51:41 +0800

    fix: correct spelling

--- internal/dorm/db/mongo.go
+++ internal/dorm/db/mongo.go
@@ -212,7 +212,7 @@ func (d *Dorm) Count(ctx context.Context) (int64, error) {
 	return d.C.CountDocuments(ctx, d.builder.Vars)
 }
 func (d *Dorm) Insert(ctx context.Context, entities ...interface{}) (int64, error) {
-	ret, err := d.C.InsertMany(ctx, entirties)
+	ret, err := d.C.InsertMany(ctx, entities)
 	return int64(len(ret.InsertedIDs)), err
 }
 func (d *Dorm) Update(ctx context.Context, entity interface{}) (int64, error) {
