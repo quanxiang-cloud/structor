0280b22ccbaf22cc0de6db324963fbed5a866b85
hejiaming <jameshe@yunify.com>
2022-01-24 15:02:30 +0800

    feature:ddl add and modify column functions

--- internal/dorm/db/definition_mysql.go
+++ internal/dorm/db/definition_mysql.go
@@ -31,7 +31,7 @@ func add() structor.Constructor {
 }
 
 func (a *Add) Build(builder structor.Builder) {
-	builder.WriteRaw(fmt.Sprintf("ALTER TABLE %s ADD COLUMN ", a.Column))
+	builder.WriteRaw(fmt.Sprintf(" ALTER TABLE %s ADD COLUMN ", a.Column))
 	builder.WriteRaw(a.Values.Convert())
 	builder.WriteRaw(";")
 }
@@ -45,7 +45,7 @@ func modify() structor.Constructor {
 }
 
 func (m *Modify) Build(builder structor.Builder) {
-	builder.WriteRaw(fmt.Sprintf("ALTER TABLE %s MODIFY COLUMN ", m.Column))
+	builder.WriteRaw(fmt.Sprintf(" ALTER TABLE %s MODIFY COLUMN ", m.Column))
 	builder.WriteRaw(m.Values.Convert())
 	builder.WriteRaw(";")
 }
