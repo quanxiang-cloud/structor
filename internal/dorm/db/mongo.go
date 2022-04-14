ad2acfd7377fcd0bf4cafa525d85bd409b6ce190
minhjh <mingmj7557@gmail.com>
2022-04-14 14:30:41 +0800

    fix:fix ping mongo and find return count

--- internal/dorm/db/mongo.go
+++ internal/dorm/db/mongo.go
@@ -17,6 +17,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var _ clause.Builder = &MONGO{}
@@ -118,6 +119,12 @@ func New() (*Dorm, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	err = client.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+
 	return &Dorm{
 		db: client.Database(database),
 	}, nil
